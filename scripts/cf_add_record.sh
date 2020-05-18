e46382fe46745ebfaf4490810a8e257a13e9c4a2
Martin Beránek <martin.beranek112@gmail.com>
2020-05-18 15:35:27 +0200

    fix: missing zone id in url

--- scripts/cf_add_record.sh
+++ scripts/cf_add_record.sh
@@ -30,7 +30,7 @@ if [[ -z "${DOMAIN_NAME}" ]]; then
     exit 1
 fi
 
-if [[ $(curl -X GET "https://api.cloudflare.com/client/v4/zones/53cfc44431c37e9d5d93db044c7a8bdb/dns_records" \
+if [[ $(curl -X GET "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
      -H "X-Auth-Key: ${DOMAIN_KEY}" \
      -H "X-Auth-Email: ${DOMAIN_EMAIL}" \
      -H "Content-Type: application/json" | \

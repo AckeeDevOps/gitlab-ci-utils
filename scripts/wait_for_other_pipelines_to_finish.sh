9b8da5d3e33287caf492d5c683336442301856cf
Sandra Tatarevićová <sandra.tatarevicova@ackee.cz>
2020-06-02 13:14:16 +0200

    🎨 Refactor the script, fix some minor bugs
    Related: #46504

--- scripts/wait_for_other_pipelines_to_finish.sh
+++ scripts/wait_for_other_pipelines_to_finish.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 
-if [[ -z "${CI_PROJECT_ID}" ]]; then
-    echo -e "\033[0;31mERROR: Variable CI_PROJECT_ID is missing, are you using the script in gitlab ci pipeline?!\033[0m" >&2
-    exit 1
+set -eu -o pipefail
+
+fail() {
+  echo -e "\033[0;31mERROR: $1\033[0m" >&2
+  exit 1
+}
+
+get_pipelines() {
+  curl -sS -H "Private-Token: $SECRET_GITLAB_ACCESS_TOKEN" "${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines?sha=${CI_COMMIT_SHA}" | jq -r
+}
+
+if [[ -z "${CI_PROJECT_ID-}" ]]; then
+    fail "Variable CI_PROJECT_ID is missing, are you using the script in gitlab ci pipeline?!"
 fi
-if [[ -z "${CI_PIPELINE_ID}" ]]; then
-    echo -e "\033[0;31mERROR: Variable CI_PIPELINE_ID is missing, are you using the script in gitlab ci pipeline?!\033[0m" >&2
-    exit 1
+if [[ -z "${CI_SERVER_URL-}" ]]; then
+    fail "Variable CI_SERVER_URL is missing, are you using the script in gitlab ci pipeline?!"
 fi
-if [[ -z "${CI_SERVER_URL}" ]]; then
-    echo -e "\033[0;31mERROR: Variable CI_SERVER_URL is missing, are you using the script in gitlab ci pipeline?!\033[0m" >&2
-    exit 1
+if [[ -z "${CI_COMMIT_SHA-}" ]]; then
+    fail "Variable CI_COMMIT_SHA is missing, are you using the script in gitlab ci pipeline?!"
 fi
-if [[ -z "${SECRET_GITLAB_ACCESS_TOKEN}" ]]; then
-    echo -e "\033[0;31mERROR: Variable SECRET_GITLAB_ACCESS_TOKEN is missing, set it up to allow runner fetch data from gitlab ci api\033[0m" >&2
-    exit 1
+if [[ -z "${SECRET_GITLAB_ACCESS_TOKEN-}" ]]; then
+    fail "Variable SECRET_GITLAB_ACCESS_TOKEN is missing, set it up to allow runner fetch data from gitlab ci api"
 fi
 
 printf 'Waiting...'
-SHA=$(curl -sS --header "PRIVATE-TOKEN: $SECRET_GITLAB_ACCESS_TOKEN" "${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}" | jq -r .sha)
-while [[ $(curl -sS --header "PRIVATE-TOKEN: $SECRET_GITLAB_ACCESS_TOKEN" "${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines" | jq -r "map(select(.sha == \"$SHA\"))" | grep running | wc -l) -gt 1 ]]; do
+while [[ $(get_pipelines | grep running | wc -l) -gt 1 ]]; do
     printf '.'
     sleep 10
 done
+echo
 
-curl -sS --header "PRIVATE-TOKEN: $SECRET_GITLAB_ACCESS_TOKEN" "${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines" | jq -r "map(select(.sha == \"$SHA\"))"
-if curl -sS --header "PRIVATE-TOKEN: $SECRET_GITLAB_ACCESS_TOKEN" "${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines" | jq -r "map(select(.sha == \"$SHA\"))" | grep "failed"; then
-    echo -e "\033[0;31mERROR: Other pipeline in this merge request group already failed, check their status!\033[0m" >&2
-    exit 1
+pipelines=$(get_pipelines)
+echo "$pipelines"
+if grep -q "failed" <<< "$pipelines"; then
+    fail "Other pipeline in this merge request group already failed, check their status!"
 fi

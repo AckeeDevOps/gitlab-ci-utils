294549131f730e7ab0198524d128dca93fc868e2
Tibor Tribus <tibor.tribus@ackee.cz>
2021-03-29 08:37:38 +0200

    ✨ Add delete record script
    related: #56959

---
+++ scripts/cf_delete_record.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ -z "${DOMAIN_ZONE_ID}" ]]; then
+    echo -e "\033[0;31mERROR: Variable DOMAIN_ZONE_ID is missing, set it up in CI settings.\033[0m" >&2
+    exit 1
+fi
+
+if [[ -z "${DOMAIN_KEY}" ]]; then
+    echo -e "\033[0;31mERROR: Variable DOMAIN_KEY is missing, set it up in CI settings.\033[0m" >&2
+    exit 1
+fi
+
+if [[ -z "${DOMAIN_EMAIL}" ]]; then
+    echo -e "\033[0;31mERROR: Variable DOMAIN_EMAIL is missing, set it up in CI settings.\033[0m" >&2
+    exit 1
+fi
+
+if [[ -z "${DOMAIN_CONTENT}" ]]; then
+    echo -e "\033[0;31mERROR: Variable DOMAIN_CONTENT is missing, set it up in CI settings.\033[0m" >&2
+    exit 1
+fi
+
+DNS_RECORD_ID=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/0da3f10ded5b96d4145d94c3415c9777/dns_records" \
+     -H "X-Auth-Key: 84d09b4b57f4e26825e408bb1e9b7e6416dec" \
+     -H "X-Auth-Email: admin@ackee.cz" \
+     -H "Content-Type: application/json" | \
+     jq -r ".result | map(select(.zone_id == \"${DOMAIN_ZONE_ID}\")) | map(select(.type == \"${DOMAIN_RECORD_TYPE}\")) | map(select(.content == \"${DOMAIN_CONTENT}\")) | .[].id")
+
+if [[ -z "${DNS_RECORD_ID}" ]]; then
+    echo -e "\033[0;31mWARNING: Deleting a CloudFlare DNS record ${DOMAIN_CONTENT} skipped, record doesn't exists\033[0m" >&2
+    exit 1
+else
+    curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records/${DNS_RECORD_ID}" \
+        -H "Content-Type:application/json" \
+        -H "X-Auth-Key: ${DOMAIN_KEY}" \
+        -H "X-Auth-Email: ${DOMAIN_EMAIL}" | \
+        jq ".result | map(select(.id == \"${DOMAIN_ZONE_ID}\"))"
+fi
+

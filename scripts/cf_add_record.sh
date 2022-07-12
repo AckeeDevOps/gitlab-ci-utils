7f0db15bb23c55ae9ef6cfe35772f679076d12dc
Tomáš Hejátko <tomas.hejatko@ackee.cz>
2022-07-12 09:17:47 +0200

    ✨ Support adding and deleting CF records with CF API key
    Related: #73222

--- scripts/cf_add_record.sh
+++ scripts/cf_add_record.sh
@@ -13,13 +13,8 @@ if [[ -z "${DOMAIN_ZONE_ID}" ]]; then
     exit 1
 fi
 
-if [[ -z "${DOMAIN_KEY}" ]]; then
-    echo -e "\033[0;31mERROR: Variable DOMAIN_KEY is missing, set it up in CI settings.\033[0m" >&2
-    exit 1
-fi
-
-if [[ -z "${DOMAIN_EMAIL}" ]]; then
-    echo -e "\033[0;31mERROR: Variable DOMAIN_EMAIL is missing, set it up in CI settings.\033[0m" >&2
+if [[ -z "${DOMAIN_KEY}" && -z "${DOMAIN_TOKEN}" ]]; then
+    echo -e "\033[0;31mERROR: Variable DOMAIN_KEY and DOMAIN_TOKEN are missing, set one of it in CI settings.\033[0m" >&2
     exit 1
 fi
 
@@ -33,22 +28,44 @@ if [[ -z "${DOMAIN_NAME}" ]]; then
     exit 1
 fi
 
-if [[ $(curl -X GET "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
-     -H "X-Auth-Key: ${DOMAIN_KEY}" \
-     -H "X-Auth-Email: ${DOMAIN_EMAIL}" \
-     -H "Content-Type: application/json" | \
-     jq ".result | map(select(.zone_id == \"${DOMAIN_ZONE_ID}\")) | map(select(.type == \"${DOMAIN_RECORD_TYPE}\")) | map(select(.content == \"${DOMAIN_CONTENT}\"))") == "[]" ]]; then
+if [[ -n "${DOMAIN_KEY}" ]]; then
+  if [[ -z "${DOMAIN_EMAIL}" ]]; then
+      echo -e "\033[0;31mERROR: Variable DOMAIN_EMAIL is missing, set it up in CI settings.\033[0m" >&2
+      exit 1
+  fi
+  if [[ $(curl -X GET "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
+       -H "X-Auth-Key: ${DOMAIN_KEY}" \
+       -H "X-Auth-Email: ${DOMAIN_EMAIL}" \
+       -H "Content-Type: application/json" | \
+       jq ".result | map(select(.zone_id == \"${DOMAIN_ZONE_ID}\")) | map(select(.type == \"${DOMAIN_RECORD_TYPE}\")) | map(select(.content == \"${DOMAIN_CONTENT}\"))") == "[]" ]]; then
 
-    curl -X POST "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
-        -H "Content-Type:application/json" \
-        -H "X-Auth-Key: ${DOMAIN_KEY}" \
-        -H "X-Auth-Email: ${DOMAIN_EMAIL}" \
-        --data "{\"type\":\"${DOMAIN_RECORD_TYPE}\",\"name\":\"${DOMAIN_NAME}\",\"content\":\"${DOMAIN_CONTENT}\",\"ttl\":1,\"proxied\":${DOMAIN_PROXIED}}" 2>/dev/null \
-        | jq .success \
-        | grep true \
-    || echo -e "\033[0;31mERROR: Creating a CloudFlare DNS type ${DOMAIN_RECORD_TYPE} record ${DOMAIN_NAME} failed\033[0m" >&2
+      curl -X POST "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
+          -H "Content-Type:application/json" \
+          -H "X-Auth-Key: ${DOMAIN_KEY}" \
+          -H "X-Auth-Email: ${DOMAIN_EMAIL}" \
+          --data "{\"type\":\"${DOMAIN_RECORD_TYPE}\",\"name\":\"${DOMAIN_NAME}\",\"content\":\"${DOMAIN_CONTENT}\",\"ttl\":1,\"proxied\":${DOMAIN_PROXIED}}" 2>/dev/null \
+          | jq .success \
+          | grep true \
+      || echo -e "\033[0;31mERROR: Creating a CloudFlare DNS type ${DOMAIN_RECORD_TYPE} record ${DOMAIN_NAME} failed\033[0m" >&2
 
-else
-    echo -e "\033[0;31mWARNING: Creating a CloudFlare DNS type ${DOMAIN_RECORD_TYPE} record skipped, record already in place\033[0m" >&2
+  else
+      echo -e "\033[0;31mWARNING: Creating a CloudFlare DNS type ${DOMAIN_RECORD_TYPE} record skipped, record already in place\033[0m" >&2
+  fi
+elif [[ -n "${DOMAIN_TOKEN}" ]]; then
+  if [[ $(curl -X GET "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
+       -H "Authorization: Bearer ${DOMAIN_TOKEN}" \
+       -H "Content-Type: application/json" | \
+       jq ".result | map(select(.zone_id == \"${DOMAIN_ZONE_ID}\")) | map(select(.type == \"${DOMAIN_RECORD_TYPE}\")) | map(select(.content == \"${DOMAIN_CONTENT}\"))") == "[]" ]]; then
+
+      curl -X POST "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
+          -H "Content-Type:application/json" \
+          -H "Authorization: Bearer ${DOMAIN_TOKEN}" \
+          --data "{\"type\":\"${DOMAIN_RECORD_TYPE}\",\"name\":\"${DOMAIN_NAME}\",\"content\":\"${DOMAIN_CONTENT}\",\"ttl\":1,\"proxied\":${DOMAIN_PROXIED}}" 2>/dev/null \
+          | jq .success \
+          | grep true \
+      || echo -e "\033[0;31mERROR: Creating a CloudFlare DNS type ${DOMAIN_RECORD_TYPE} record ${DOMAIN_NAME} failed\033[0m" >&2
+
+  else
+      echo -e "\033[0;31mWARNING: Creating a CloudFlare DNS type ${DOMAIN_RECORD_TYPE} record skipped, record already in place\033[0m" >&2
+  fi
 fi
-
--- scripts/cf_delete_record.sh
+++ scripts/cf_delete_record.sh
@@ -10,13 +10,8 @@ if [[ -z "${DOMAIN_RECORD_TYPE}" ]]; then
     exit 1
 fi
 
-if [[ -z "${DOMAIN_KEY}" ]]; then
-    echo -e "\033[0;31mERROR: Variable DOMAIN_KEY is missing, set it up in CI settings.\033[0m" >&2
-    exit 1
-fi
-
-if [[ -z "${DOMAIN_EMAIL}" ]]; then
-    echo -e "\033[0;31mERROR: Variable DOMAIN_EMAIL is missing, set it up in CI settings.\033[0m" >&2
+if [[ -z "${DOMAIN_KEY}" && -z "${DOMAIN_TOKEN}" ]]; then
+    echo -e "\033[0;31mERROR: Variable DOMAIN_KEY and DOMAIN_TOKEN are missing, set one of it in CI settings.\033[0m" >&2
     exit 1
 fi
 
@@ -25,19 +20,42 @@ if [[ -z "${DOMAIN_CONTENT}" ]]; then
     exit 1
 fi
 
-DNS_RECORD_ID=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
-     -H "X-Auth-Key: ${DOMAIN_KEY}" \
-     -H "X-Auth-Email: ${DOMAIN_EMAIL}" \
-     -H "Content-Type: application/json" | \
-     jq -r ".result | map(select(.zone_id == \"${DOMAIN_ZONE_ID}\")) | map(select(.type == \"${DOMAIN_RECORD_TYPE}\")) | map(select(.content == \"${DOMAIN_CONTENT}\")) | .[].id")
+if [[ -n "${DOMAIN_KEY}" ]]; then
+  if [[ -z "${DOMAIN_EMAIL}" ]]; then
+      echo -e "\033[0;31mERROR: Variable DOMAIN_EMAIL is missing, set it up in CI settings.\033[0m" >&2
+      exit 1
+  fi
+  DNS_RECORD_ID=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
+       -H "X-Auth-Key: ${DOMAIN_KEY}" \
+       -H "X-Auth-Email: ${DOMAIN_EMAIL}" \
+       -H "Content-Type: application/json" | \
+       jq -r ".result | map(select(.zone_id == \"${DOMAIN_ZONE_ID}\")) | map(select(.type == \"${DOMAIN_RECORD_TYPE}\")) | map(select(.content == \"${DOMAIN_CONTENT}\")) | .[].id")
 
-if [[ -z "${DNS_RECORD_ID}" ]]; then
-    echo -e "\033[0;31mWARNING: Deleting a CloudFlare DNS record ${DOMAIN_CONTENT} skipped, record doesn't exists\033[0m" >&2
-    exit 1
-else
-    curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records/${DNS_RECORD_ID}" \
-        -H "X-Auth-Key: ${DOMAIN_KEY}" \
-        -H "X-Auth-Email: ${DOMAIN_EMAIL}" \
-        -H "Content-Type:application/json"
+  if [[ -z "${DNS_RECORD_ID}" ]]; then
+      echo -e "\033[0;31mWARNING: Deleting a CloudFlare DNS record ${DOMAIN_CONTENT} skipped, record doesn't exists\033[0m" >&2
+      exit 1
+  else
+      curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records/${DNS_RECORD_ID}" \
+          -H "X-Auth-Key: ${DOMAIN_KEY}" \
+          -H "X-Auth-Email: ${DOMAIN_EMAIL}" \
+          -H "Content-Type:application/json"
+  fi
+elif [[ -n "${DOMAIN_TOKEN}" ]]; then
+  if [[ -z "${DOMAIN_EMAIL}" ]]; then
+      echo -e "\033[0;31mERROR: Variable DOMAIN_EMAIL is missing, set it up in CI settings.\033[0m" >&2
+      exit 1
+  fi
+  DNS_RECORD_ID=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records" \
+       -H "Authorization: Bearer ${DOMAIN_TOKEN}" \
+       -H "Content-Type: application/json" | \
+       jq -r ".result | map(select(.zone_id == \"${DOMAIN_ZONE_ID}\")) | map(select(.type == \"${DOMAIN_RECORD_TYPE}\")) | map(select(.content == \"${DOMAIN_CONTENT}\")) | .[].id")
+
+  if [[ -z "${DNS_RECORD_ID}" ]]; then
+      echo -e "\033[0;31mWARNING: Deleting a CloudFlare DNS record ${DOMAIN_CONTENT} skipped, record doesn't exists\033[0m" >&2
+      exit 1
+  else
+      curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ZONE_ID}/dns_records/${DNS_RECORD_ID}" \
+         -H "Authorization: Bearer ${DOMAIN_TOKEN}" \
+         -H "Content-Type:application/json"
+  fi
 fi
-
